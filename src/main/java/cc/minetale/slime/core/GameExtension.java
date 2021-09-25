c8b01c80c65fa1e587c4b027e9ac61bc4e362f7b
Matthew <matik04@gmail.com>
2021-09-25 20:02:55 +0200

    Move TeamProvider to GameExtension

--- src/main/java/cc/minetale/slime/core/GameExtension.java
+++ src/main/java/cc/minetale/slime/core/GameExtension.java
@@ -1,7 +1,10 @@
 package cc.minetale.slime.core;
 
+import cc.minetale.slime.team.GameTeam;
 import net.minestom.server.extensions.Extension;
 import net.minestom.server.network.PlayerProvider;
+
+import java.util.function.Supplier;
 
 public abstract class GameExtension extends Extension {
 
@@ -9,6 +12,7 @@ public abstract class GameExtension extends Extension {
     public abstract String getName();
 
     public abstract PlayerProvider getPlayerProvider();
+    public abstract Supplier<? extends GameTeam> getTeamProvider();
 
     public abstract int getMinPlayers();
     public abstract int getMaxPlayers();
--- src/main/java/cc/minetale/slime/team/TeamAssigner.java
+++ src/main/java/cc/minetale/slime/team/TeamAssigner.java
@@ -1,40 +1,37 @@
 package cc.minetale.slime.team;
 
+import cc.minetale.slime.Slime;
 import cc.minetale.slime.core.Game;
 import cc.minetale.slime.core.GamePlayer;
 
 import java.util.*;
-import java.util.function.Supplier;
 
 public abstract class TeamAssigner {
 
     /**
      * Assigns players to each team.
      * @param game Game the players are being assigned for
-     * @param teamProvider Team provider
      * @param players Players to assign
-     * @param <T> {@linkplain GameTeam} implementation to use
      * @param <P> {@linkplain GamePlayer} implementation to use
      * @return Players assigned to their teams. <br>
      * Can be used to do intermediate operations on players based on their teams before <br>
      * actually assigning them, usually through {@linkplain TeamAssigner#assignTeams(Map)}.
      */
-    public abstract <T extends GameTeam, P extends GamePlayer> Map<T, Set<P>> assign(Game game,
-                                                                                     List<ITeamType> availableTeams,
-                                                                                     Supplier<T> teamProvider,
-                                                                                     List<P> players);
+    public abstract <P extends GamePlayer> Map<GameTeam, Set<P>> assign(Game game,
+                                                                        List<ITeamType> availableTeams,
+                                                                        List<P> players);
 
     public static TeamAssigner simple(int teamSize) {
         return new TeamAssigner() {
-            @Override public <T extends GameTeam, P extends GamePlayer> Map<T, Set<P>> assign(Game game,
-                                                                                              List<ITeamType> availableTeams,
-                                                                                              Supplier<T> teamProvider,
-                                                                                              List<P> players) {
+            @Override public <P extends GamePlayer> Map<GameTeam, Set<P>> assign(Game game,
+                                                                                 List<ITeamType> availableTeams,
+                                                                                 List<P> players) {
+                Map<GameTeam, Set<P>> assignedTeams = new HashMap<>();
 
-                Map<T, Set<P>> assignedTeams = new HashMap<>();
+                players = new ArrayList<>(players);
 
                 for(ITeamType teamType : availableTeams) {
-                    var team = teamProvider.get();
+                    var team = Slime.getActiveGame().getTeamProvider().get();
 
                     team.setType(teamType);
                     team.setSize(teamSize);
