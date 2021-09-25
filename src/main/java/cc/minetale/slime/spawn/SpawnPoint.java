bf3cae374c7609fc500ba401aaa99061c441f02c
Matthew <matik04@gmail.com>
2021-09-26 01:11:07 +0200

    SpawnPoint now requires a position and an instance

--- src/main/java/cc/minetale/slime/spawn/SpawnPoint.java
+++ src/main/java/cc/minetale/slime/spawn/SpawnPoint.java
@@ -5,28 +5,45 @@ import lombok.Getter;
 import lombok.Setter;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.instance.Instance;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
-public class SpawnPoint {
+public final class SpawnPoint {
 
     private final Set<GameTeam> owners;
-    @Setter private Pos position;
-    @Setter private Instance instance;
+    @Setter @NotNull private Pos position;
+    @Setter @NotNull private Instance instance;
 
-    public SpawnPoint() {
+    public SpawnPoint(@NotNull Pos position, @NotNull Instance instance) {
         this.owners = Collections.synchronizedSet(new HashSet<>());
+        this.position = position;
+        this.instance = instance;
     }
 
-    public SpawnPoint(GameTeam owner) {
-        this.owners = Collections.synchronizedSet(new HashSet<>(Collections.singleton(owner)));
+    public SpawnPoint(GameTeam owner, @NotNull Pos position, @NotNull Instance instance) {
+        this(Collections.singleton(owner), position, instance);
     }
 
-    public SpawnPoint(Set<GameTeam> owners) {
+    public SpawnPoint(Set<GameTeam> owners, @NotNull Pos position, @NotNull Instance instance) {
         this.owners = Collections.synchronizedSet(new HashSet<>(owners));
+        this.position = position;
+        this.instance = instance;
+    }
+
+    public boolean addOwner(GameTeam team) {
+        return this.owners.add(team);
+    }
+
+    public boolean removeOwner(GameTeam team) {
+        return this.owners.remove(team);
+    }
+
+    public boolean isShared() {
+        return this.owners.size() > 1;
     }
 
 }
